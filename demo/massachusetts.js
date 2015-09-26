b7ca971187ecedf00188d32ecde851470ffbd298
Chris Bolin <bolin.chris@gmail.com>
2015-09-26 11:34:39 -0400

    mass townname demo

---
+++ demo/massachusetts.js
@@ -0,0 +1,15 @@
+var Chain = require('../index.js');
+
+var names = require('./massachusetts.json').names;
+
+var chain = new Chain(3);
+
+chain.push(names);
+
+for (var i = 0; i < 100; i++) {
+  var name = chain.generate();
+  var exists = (chain.list.indexOf(name) > 0);
+  if (!exists) {
+    console.log(name);
+  }
+}
